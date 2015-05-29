20d2822cba4064a57da0d0ff3bab37810bc3809b
Alan Moran <bonzofenix@gmail.com>
2015-05-29 12:33:34 -0700

    fix on missing font

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 node.default['sprout']['terminal']['default_profile'] = 'Basic'
-node.default['sprout']['terminal']['font_src'] = 'http://cheffiles.pivotallabs.com/MenloRegular18pt.bin'
+node.default['sprout']['terminal']['font_src'] = 'https://gist.github.com/bonzofenix/aa67e404d42fe82b00c2/raw/7219e36d2df8f0a7ebee81ecca52164f78937291/MenloRegular18pt'
